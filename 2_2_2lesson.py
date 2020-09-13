332563a3b15087cbe6927e44d80d82b012fb04f4
Ksumat <Ksusha.matuhsa@gmail.com>
2020-09-13 15:10:36 +0300

    Добавлен урок

---
+++ 2_2_2lesson.py
@@ -0,0 +1,25 @@
+import math
+import time
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+link = 'http://suninjuly.github.io/alert_accept.html'
+browser.get(link)
+
+submit = browser.find_element_by_tag_name("button").click()
+confirm = browser.switch_to.alert
+confirm.accept()
+
+x_element = browser.find_element_by_css_selector("#input_value")
+x = x_element.text
+y = calc(x)
+input = browser.find_element_by_css_selector("#answer")
+input.send_keys(y)
+
+button = browser.find_element_by_tag_name('button').click()
+
+
+time.sleep(10)
+browser.quit()
